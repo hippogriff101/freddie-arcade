c45ddf27b8999a1b5094828badd8943d72f42e59
yersf0001 <143739931+yersf0001@users.noreply.github.com>
2024-06-29 14:45:10 +0100

    Update #NeoCity, 2142.py to #NeoCity, 2142 - v2.py

--- Text Game/#NeoCity, 2142 - v2.py
+++ Text Game/#NeoCity, 2142 - v2.py
@@ -1,8 +1,11 @@
 #NeoCity, 2142
 
+import random
+import time
+
 rooms = ["Your Hideout", "The Market", "The Underpass", "The Mainframe", "The Exit"]
 
-itemsgained = ["Map", "Keycard", "Energy Drink"]
+itemsgained = ["Map", "Keycard", "Energy Drink", "Smoke Bomb", "Laser Cutter", "Hoverboard"]
 
 itemsneeded = ["AI Assistant", "Blaster"]
 
@@ -11,6 +14,7 @@ items = [itemsgained, itemsneeded]
 inv = [itemsneeded,]
 
 def hideout():
+
     print("Welcome to your hideout, this is your control center.")
     print("Once you leave here you cant come back until the end of your mission")
     print("I'm your AI assistant, I will be guiding you through your mission.")
@@ -18,10 +22,37 @@ def hideout():
 
     print("At points during the mission I will ask you if you want to see your inventry")
 
+    print("")
+
     invopen = input("Do you want to open your backpack? (press 'e')")
 
     if invopen == 'e':
         print(inv)
+    else:
+        print("Ok then")
+
+    print("")
 
     print("Now you are going to get a random item you can use to complete your task")
 
+    itemrandom = random.choice(itemsgained)
+    itemsgained.remove(itemrandom)
+    print("You got a", itemrandom)
+
+    print("Now your going to go out and escape from this blasted city!")
+
+    avaliblerooms = ("Market", "Underpass")
+
+    nextroom = random.choice(avaliblerooms)
+
+    print("Your going to go to the", nextroom)
+   
+def loading():
+        
+        load = ("Loading... \n")
+        for i in range(4):
+            for char in load:
+                print(char, end='', flush=True)
+                time.sleep(0.1)
+
+hideout()
